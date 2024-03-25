9f289af642d09b4dad2142575e192de410cc855d
ywang0613 <163805790+ywang0613@users.noreply.github.com>
2024-03-25 12:42:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main", methods=["GET","POST"])
+def main():
+    name = request.form.get("name")
+    return(render_template("main.html",r=name))
+
 @app.route("/palm", methods=["GET","POST"])
 def palm_flask():
     return(render_template("palm.html"))
